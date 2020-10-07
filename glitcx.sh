a9d6e2ea20b9c12fcb2c2865dc13cc3ebd6a1a30
mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-10-07 11:55:36 -0400

    Update glitcx.sh

--- glitcx.sh
+++ glitcx.sh
@@ -27,13 +27,13 @@ elif [[ $1 == "-edit" ]] ; then
   echo -e "\e[1;41m ctrl C to exit \e[0m"
   cat > $2
 elif [[ $1 == "-clone-folder" ]] ; then
-  cd ..
+  rm * && rmdir *
   wget $2
-  rmdir app
-  mv $3 "app"
+  #$3foldername
+  cd /app/$3 && mv * ../
   refresh
 elif [[ $1 == "-git-clone-app" ]] ; then
-  cd .. && git clone $2/$3 && rm -r app && mv $3 app && refresh # oneliner github import
+  cd /app/ && rm -r * && git clone $2/$3 && cd $3 && mv * ../ # oneliner github import $2/$3
 elif [[ $1 == "-nodify" ]] ; then
   mkdir /tmp/app
   cp -r /app/* /tmp/app/
@@ -45,10 +45,10 @@ elif [[ $1 == "-nodify" ]] ; then
   curl https://glitcxcli.glitch.me/default-node.tar.bz2 | tar xjf -x
   #original from  nodify.glitch.me/default-node.tar.bz2
   refresh
-elif [[ $1 == "-restart" ]] ; then
+elif [[ $1 == "-delete" ]] ; then
   echo 'sure? close if not.';
   read var
-  cd /app/ && rm *
+  cd /app/ && rm -r *
   refresh
 elif [[ $1 == "-uninstall" ]] ; then
   echo 'sure? close if not. will also delete .bash_profile';
@@ -70,13 +70,22 @@ elif [[ $1 == "-secure-npm" ]] ; then
   if [[ "$2" == *"$z"* ]]; then
     echo ""
   else
-    echo "package may be insecure"
+    echo "package may be insecure, continue?"
+    read pkh
   fi
 elif [[ $1 == "-installdeps" ]] ; then
-  pip install pip-safe && npm install npm@latest -g
+  pip install pip-safe && npm install npm@latest -g && python -m pip install –upgrade pip
 elif [[ $1 == "-secure-pip" ]] ; then
+  z="coffescript typescript react request urllib2express tslib react-dom axios webpack random pandas numpy"
+  if [[ "$2" == *"$z"* ]] ; then
+    echo ""
+  else
+    echo "package may be insecure, continue?"
+    read pkh
+  fi
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py
   pip-safe install $2 --verbose
+
 elif [[ $1 == "-help" ]] ; then
   echo '-clone_link link foldername imports from a folder';
   echo '-edit file                  the worst shell editor';
@@ -87,12 +96,14 @@ elif [[ $1 == "-help" ]] ; then
   echo '-glitcx-commandline         enter glitcx command line';
   echo '-help                       show this help message';
   echo '-nodify                     nodify project';
-  echo '-restart                    delete all contents in app';
+  echo '-delete                     delete all contents in app';
   echo '-uninstall                  uninstall glitcx cli';
   echo '-zip-it                     zip this project';
-  echo 'Package managers (dont work yet)'
+  echo 'Package managers (may be buggy)'
   echo '-secure-npm package         securely install from NPM'
   echo '-secure-pip package         securely install from PIP'
   echo 'Install dependencies that may not be installed via'
   echo './glitch-bleeding-edge.sh -installdeps'
 fi
+
+echo "# User executed $0 at $(date) on glitcx." >> /app/.env
